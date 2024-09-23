b9ca77ed6689d268efebde5f32f286bf6dec3cd0
noLEAL <brunopazet@gmail.com>
2024-09-23 08:48:17 -0300

    arrumando projeto aula

---
+++ DEV 1/pythonProject/OrdenacaoBinaria.py
@@ -0,0 +1,14 @@
+import random
+agenda = []
+
+def pesquisabinaria(estrutura: list):
+    return
+
+for i in range(100):
+    #item +=1
+    item = random.randint(1,1000)
+    agenda.append(item)
+
+print(agenda)
+agendaord = sorted(agenda)
+print(agendaord)
