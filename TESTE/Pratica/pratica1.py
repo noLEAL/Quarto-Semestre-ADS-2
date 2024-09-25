6dd2e21f6e7f7a94f1898a5d54911ae0503ecb33
noLEAL <brunopazet@gmail.com>
2024-09-25 10:28:41 -0300

    aula irisoria

---
+++ TESTE/Pratica/pratica1.py
@@ -0,0 +1,14 @@
+
+lado1 = input('Digite o primeiro (a) número:')
+lado2 = input('Digite o segundo (b) número:')
+ = input('Digite o terceiro (c) número:')
+
+if lado1 != lado2 and lado2 != lado3 and lado1 != lado3:
+    print("É um triângulo escaleno.")
+    print('Nao é um triangulo')
+elif (a == b) and (a == c):
+    print('Equilatero')
+elif (a == b) or (a == c) or (b == c):
+    print('Isósceles')
+else:
+    print('Escaleno')
