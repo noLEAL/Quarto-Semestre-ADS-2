b0c6cef29568c502d57776410b3a881f2797f966
noLEAL <brunopazet@gmail.com>
2024-10-19 14:27:04 -0300

    programa de meu mercado TESTE

--- TESTE/MeuMercado/main.py
+++ TESTE/MeuMercado/main.py
@@ -19,46 +19,65 @@ def menu():
 
         opcao = input("\nEscolha uma opção: ").upper()
 
-        match opcao:
+        try:
+            match opcao:
 
-            case 'ESC':
-                sistema.basedate()
+                case 'ESC':
+                    sistema.basedate()
 
-            case '1':
-                print("▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓")
-                print("Opção 1 Listar produtos ")
-                sistema.view_products()
-                print("Produtos Listados acima ^")
-                print("▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓")
-            case '2':
-                print("Opção 2 Busca de produtos (nome)")
+                case '1':
+                    print("▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓")
+                    print("Opção 1 Listar produtos ")
+                    sistema.view_products()
+                    print("Produtos Listados acima ^")
+                    print("▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓")
+                case '2':
+                    print("▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓")
+                    print("Opção 2 Busca de produtos (nome)")
+                    local_product = input("Digite o nome do produto que você gostaria de pesquisar?")
+                    sistema.search_product_name(local_product)
+                    print("▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓")
+                case '3':
+                    print("▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓")
+                    print("Opção 3 Busca de produtos (tipo)")
+                    local_product = input("Digite o tipo de produto que você gostaria de pesquisar?")
+                    sistema.search_product_name(local_product)
+                    print("▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓")
+                case '4':
+                    print("▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓")
+                    print("Opção 4 Cadastrar produtos")
+                    local_nome = input("Digite o nome do produto que deseja cadastrar:")
+                    local_tipo = input("Digite o tipo do produto que deseja cadastrar:")
+                    local_valor = input("Digite o valor do produto que deseja cadastrar:")
+                    local_estoque = input("Digite o estoque do produto que deseja cadastrar:")
+                    sistema.register_product(local_nome, local_tipo, local_valor, local_estoque)
+                    print("▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓")
+                case '5':
+                    print("▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓")
+                    print("Opção 5 Excluir produto")
+                    codigo = input(id("Digite o codigo do produto que deseja excluir:"))
+                    sistema.delete_product(codigo)
+                    print("Produto excluido acima ^")
+                    print("▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓")
+                case '6':
+                    print("▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓")
+                    print("Opção 6 Alterar valor de produto")
+                    local_id = input("Digite o codigo do produto que deseja cadastrar:")
+                    local_valor = input("Digite o valor do produto que deseja cadastrar:")
+                    sistema.update_product(local_id, local_valor)
+                    print("▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓")
+                case '7':
+                    print("▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓")
+                    print("Opção 7 Alterar estoque de produto")
+                    print("▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓")
+                case 'SAIR':
+                    print("\nEncerrando o programa...")
+                    loop = 1
 
-            case '3':
-                print("Opção 3 Busca de produtos (tipo)")
-
-            case '4':
-                print("Opção 4 Cadastrar produtos")
-
-            case '5':
-                print("▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓")
-                print("Opção 5 Excluir produto")
-                codigo = input(id("Digite o codigo do produto que deseja excluir:"))
-                sistema.delete_product(codigo)
-                print("Produto excluido acima ^")
-                print("▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓")
-
-            case '6':
-                print("Opção 6 Alterar valor de produto")
-
-            case '7':
-                print("Opção 7 Alterar estoque de produto")
-
-            case 'SAIR':
-                print("\nEncerrando o programa...")
-                loop = 1
-
-            case _:
-                print("Opção inválida")
+                case _:
+                    print("Opção inválida")
+        except Exception as e:
+            print(f"Ocorreu um erro: {e}")
 
 # Iniciar o programa
 sistema = Sistemaderegistro()
--- TESTE/MeuMercado/mercado_db.py
+++ TESTE/MeuMercado/mercado_db.py
@@ -1,4 +1,8 @@
 import sqlite3
+
+from django.utils.text import camel_case_to_spaces
+
+
 #import messagebox
 
 
@@ -9,11 +13,15 @@ class Sistemaderegistro:
     """
     def __init__(self):
         """Conecta ao banco de dados (ou cria um novo se ele não existir)"""
-        self.conn = sqlite3.connect('mercado.db')
-        self.c = self.conn.cursor()
-        self.create_table()
+        try:
+            self.conn = sqlite3.connect('mercado.db')
+            self.c = self.conn.cursor()
+            self.create_table()
+        except sqlite3.Error as e:
+            print(f"Erro ao conectar ao banco: {e}")
 
     def create_table(self):
+
         self.c.execute('''CREATE TABLE IF NOT EXISTS produtos (
                             id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
                             nome TEXT NOT NULL,
@@ -23,58 +31,98 @@ class Sistemaderegistro:
 
     def basedate(self):
         """Lista de produtos para inserir no banco de dados (dados padrão)"""
-        produtos = [
-            ("Itaipava", "Cerveja", 1.50, 200),
-            ("Skol", "Cerveja", 1.70, 300),
-            ("Del Vale", "Suco", 2.10, 100),
-            ("Pepsi Cola 2L", "Refrigerante", 3.00, 800),
-            ("Guaraná Charrua 2L", "Refrigerante", 2.50, 340),
-            ("Bis branco", "Chocolate", 2.70, 50)
-        ]
+        try:
+            produtos = [
+                ("Itaipava", "Cerveja", 1.50, 200),
+                ("Skol", "Cerveja", 1.70, 300),
+                ("Del Vale", "Suco", 2.10, 100),
+                ("Pepsi Cola 2L", "Refrigerante", 3.00, 800),
+                ("Guaraná Charrua 2L", "Refrigerante", 2.50, 340),
+                ("Bis branco", "Chocolate", 2.70, 50)
+            ]
 
-        # Executa a inserção de todos os produtos
-        self.c.executemany("INSERT INTO produtos(nome, tipo, valor, estoque) VALUES (?,?,?,?)", produtos)
-        self.conn.commit()
+            # Executa a inserção de todos os produtos
+            self.c.executemany("INSERT INTO produtos(nome, tipo, valor, estoque) VALUES (?,?,?,?)", (produtos,))
+            self.conn.commit()
 
-        # Mensagem de sucesso
-        print("Produtos padrão cadastrados com sucesso")
-        #messagebox.showinfo('Sucesso', 'Produtos padrão cadastrados com sucesso')
+            # Mensagem de sucesso
+            print("Produtos padrão cadastrados com sucesso")
+            #messagebox.showinfo('Sucesso', 'Produtos padrão cadastrados com sucesso')
+        except sqlite3.Error as e:
+            print(f"Erro ao insser produtos padrão: {e}")
 
-    def register_product(self, produto):
+    def register_product(self, nome, tipo, valor, estoque):
         """Insere um novo produto no banco de dados"""
-        self.c.execute("INSERT INTO produtos(nome, tipo, valor, estoque) VALUES (?,?,?,?)",produto)
-        self.conn.commit()
-        #messagebox.showinfo('deu certo porra', 'Produto cadastrado com sucesso')
-        print("Produto cadastrado com sucesso")
+        try:
+            self.c.execute("INSERT INTO produtos(nome, tipo, valor, estoque) VALUES (?,?,?,?)",(nome, tipo, valor, estoque,))
+            self.conn.commit()
+            #messagebox.showinfo('deu certo porra', 'Produto cadastrado com sucesso')
+            print()
+            print("Produto cadastrado com sucesso")
+        except sqlite3.Error as e:
+            print(f"Erro ao cadastrar produto: {e}")
 
     def view_products(self):
         """Seleciona todos os produtos da tabela"""
-        self.c.execute("SELECT * FROM produtos")
-        dado = self.c.fetchall()
+        try:
+            self.c.execute("SELECT * FROM produtos")
+            dado = self.c.fetchall()
 
-        for i in dado:
-            print(f'ID:{i[0]} |nome:{i[0]} |tipo:{i[0]} |valor:{i[0]} |estoque:{i[0]}')
+            for i in dado:
+                print(f'Codigo:{i[0]} | Nome:{i[1]} | Tipo:{i[2]} | Valor:R${i[3]} | Estoque:{i[4]}')
+        except sqlite3.Error as e:
+            print(f"Erro ao visualizar produtos: {e}")
 
-    def search_product(self):
-        """Busca um produto pelo ID"""
-        self.c.execute("SELECT * FROM produtos WHERE id=?", (id,))
-        dados = self.c.fetchall()
-        print(f'ID:{dados[0]} |nome:{dados[0]} |tipo:{dados[0]} |valor:{dados[0]} |estoque:{dados[0]}')
-        #messagebox.showinfo('deu certo', 'Busca realizada')
-        print("Busca realizada")
+    def search_product_name(self,name_produto):
+        """Buscar produtos pelo nome"""
+        try:
+            self.c.execute("SELECT * FROM produtos WHERE nome=?",(name_produto,))
+            dados = self.c.fetchall()
+            #print(f'ID:{dados[0]} | nome:{dados[1]} | tipo:{dados[2]} | valor:{dados[3]} | estoque:{dados[4]}')
+            for i in dados:
+                print(f'C:{i[0]} |nome:{i[1]} |tipo:{i[2]} |valor:R${i[3]} |estoque:{i[4]}')
+            print("Busca realizada")
+        except sqlite3.Error as e:
+            print(f"Erro ao procurar produto pelo nome: {e}")
 
-    def update_product(self, produto):
+    def update_product(self, produto, valor, operacao):
         """Atualiza os dados de um produto pelo ID"""
-        query = "UPDATE produtos SET nome = ?, tipo = ?, valor = ?, estoque = ? WHERE id = ?"
-        self.c.execute(query, produto)
-        self.conn.commit()
-        #messagebox.showinfo('deu certo', 'Update realizado')
-        print("Update realizado")
+        try:
+            match operacao:
+                case 'valor':
+                    query = "UPDATE produtos SET nome = ?, tipo = ?, valor = ?, estoque = ? WHERE id = ?"
+                    self.c.execute(query, produto)
+                    self.conn.commit()
+                    #messagebox.showinfo('deu certo', 'Update realizado')
+                    print("Update do valor realizado")
+
+                case 'estoque':
+                    query = "UPDATE produtos SET nome = ?, tipo = ?, valor = ?, estoque = ? WHERE id = ?"
+                    self.c.execute(query, produto)
+                    self.conn.commit()
+                    # messagebox.showinfo('deu certo', 'Update realizado')
+                    print("Update do estoque realizado")
+
+        except sqlite3.Error as e:
+            print(f"Erro ao fazer UPDATE no banco de dados: {e}")
 
     def delete_product(self,id_produto):
         """Deleta um produto pelo ID"""
-        self.c.execute("DELETE FROM produtos WHERE id=?", (id_produto,))
-        self.conn.commit()
-        #messagebox.showinfo('deu certo', 'delete realizado')
-        print("Delete realizado")
+        try:
+            self.c.execute("DELETE FROM produtos WHERE id=?", (id_produto,))
+            self.conn.commit()
+            #messagebox.showinfo('deu certo', 'delete realizado')
+            print("Delete realizado")
+        except sqlite3.Error as e:
+            print(f"Erro ao Apagar dado no banco de dados {e}")
 
+    # def search_product(self,cod_server):
+    #     """Busca um produto pelo ID"""
+    #     try:
+    #         self.c.execute("SELECT * FROM produtos WHERE id=?", (id,))
+    #         dados = self.c.fetchall()
+    #         print(f'ID:{dados[0]} |nome:{dados[1]} |tipo:{dados[2]} |valor:{dados[3]} |estoque:{dados[4]}')
+    #         #messagebox.showinfo('deu certo', 'Busca realizada')
+    #         print("Busca realizada")
+    #     except sqlite3.Error as e:
+    #         print(f"Erro ao procurar produto poelo codigo: {e}")
